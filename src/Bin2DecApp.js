a40f2ae2c3f72acbfa6c09bac9d01cb02acfe14e
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-12-09 10:59:54 -0300

    Aplica efecto para mostrar numero decimal

--- src/Bin2DecApp.js
+++ src/Bin2DecApp.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Bin2DecApp = () => {
   const [binaryNumber, setBinaryNumber] = useState("");
+  const [decimalNumber, setDecimalNumber] = useState("");
+
+  useEffect(() => {
+    if (binaryNumber) {
+      setDecimalNumber(parseInt(binaryNumber, 2));
+    } else {
+      setDecimalNumber("");
+    }
+  }, [binaryNumber]);
 
   const handleChangeBinaryNumber = ({ target }) => {
     setBinaryNumber(target.value);
   };
 
-  console.log(binaryNumber);
-
   return (
     <div>
       <input onChange={handleChangeBinaryNumber} value={binaryNumber} />
+      <p>{decimalNumber}</p>
     </div>
   );
 };
