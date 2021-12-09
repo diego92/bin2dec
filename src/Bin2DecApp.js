c609b0a81928a61d9ea4e4e22fa9de0a0f170c36
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-12-09 10:54:40 -0300

    Agrega cuadro de texto para binario

--- src/Bin2DecApp.js
+++ src/Bin2DecApp.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Bin2DecApp = () => {
-  return <div>Bin2Dec</div>;
+  const [binaryNumber, setBinaryNumber] = useState("");
+
+  const handleChangeBinaryNumber = ({ target }) => {
+    setBinaryNumber(target.value);
+  };
+
+  console.log(binaryNumber);
+
+  return (
+    <div>
+      <input onChange={handleChangeBinaryNumber} value={binaryNumber} />
+    </div>
+  );
 };
