ee44c8bd629318694fb66309bf94137ddb0be6a8
Diego Gonzalez <diego.gonzalez@hasar.com>
2021-12-09 17:05:27 -0300

    Aplica estilos de boostrap

--- src/Bin2DecApp.js
+++ src/Bin2DecApp.js
@@ -24,9 +24,21 @@ export const Bin2DecApp = () => {
   };
 
   return (
-    <div>
-      <input onChange={handleChangeBinaryNumber} value={binaryNumber} />
-      <p>{decimalNumber}</p>
+    <div className="container-fluid">
+      <div className=" text-white flex-column" style={{ height: "100vh" }}>
+        <section className="d-flex justify-content-center align-items-center flex-column h-75">
+          <h1 className="mb-5">Bin2Dec Calculator</h1>
+          <input
+            className="form-control w-50 mb-2 fw-bold"
+            onChange={handleChangeBinaryNumber}
+            value={binaryNumber}
+          />
+          <p className="fs-3 fw-bold">{decimalNumber}</p>
+        </section>
+        <footer className="text-center">
+          <p>Realizado por Diego</p>
+        </footer>
+      </div>
     </div>
   );
 };
